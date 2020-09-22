8e1a2f1f3803179a2a1f556e6df30af622b6d163
Ming Deng <mindeng@ebay.com>
2020-09-22 23:10:55 +0800

    Init

---
+++ migrate.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+find ./pkg -type f -exec sed -i '' -e 's/github.com\/astaxie\/beego/github.com\/flycash\/beego-web/g' {} \;
+
+find ./pkg -type f -exec sed -i '' -e 's/github.com\/astaxie\/beego/github.com\/flycash\/beego-web/g' {} \;
+
+
